b0c4f1f3ef775f765690cdee755ddf373a045e6d
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-03-29 03:02:44 +0200

    link fra le pagine

--- resources/js/guest/components/Header.vue
+++ resources/js/guest/components/Header.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        Header
-    </div>
+    <header>
+        <nav>
+            <!-- Link fra le pagine -->
+            <router-link :to="{ name: 'home' }">HOME</router-link>
+            <router-link :to="{ name: 'about' }">ABOUT</router-link>
+        </nav>
+    </header>
 </template>
 
 <script>
+// import Header from '../../components/Header.vue'
 
 
 export default {
+  components: { Header },
     name: "Header",
 
 }
--- resources/js/guest/pages/About.vue
+++ resources/js/guest/pages/About.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Chi siamo</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi nisi, repellat numquam, maiores corrupti quasi saepe distinctio, impedit ipsum eaque labore quam rem?</p>
     </div>
 </template>
 
